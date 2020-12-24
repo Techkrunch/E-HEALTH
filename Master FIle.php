5f39cca996acb99cf015ac90db5e871a444171a4
kilatya ian <34619999+Techkrunch@users.noreply.github.com>
2020-12-24 17:30:40 +0300

    Update Master FIle.php

--- Master FIle.php
+++ Master FIle.php
@@ -1,4 +1,8 @@
 <?php
 $variable1 ="This is a Master File";
 echo $variable1;
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
